62fd1d67ea6f2b2d343cbcdb650edfe8163c6d99
Anton Borissov <anton.borissov@softwareq.ca>
2025-07-15 13:53:16 -0400

    upd

---
+++ README.md
@@ -0,0 +1,9 @@
+# C++20 Feature
+
+[P0732R2: Class Types as Non-Type Template Parameters](https://wg21.link/p0732r2)
+
+## More references:
+
+- [C++20 feature list](https://en.cppreference.com/w/cpp/20)
+- [C++20 feature support](https://en.cppreference.com/w/cpp/20/feature_test)
+- [C++20 feature status](https://en.cppreference.com/w/cpp/20/status)
--- main.cpp
+++ main.cpp
@@ -14,14 +14,55 @@ public:
   }
 };
 
+
+class pairV {
+public:
+  std::vector<int> fst;
+  std::vector<int> snd;
+
+  pairV(std::initializer_list<int> l, std::initializer_list<int> m) : fst(l), snd(m) {}
+};
+
+class B {
+public:
+  std::vector<pairV> v;
+
+  template<typename... T>
+  requires (( sizeof...(T)%2 == 0 ) && (std::same_as<T, int> && ...))
+  B(const std::initializer_list<T>&... lists){
+    v.reserve(sizeof...(lists)/2);
+    auto lists_tuple = std::make_tuple(lists...);
+    auto create_pairs = [&]<size_t... Is>(std::index_sequence<Is...>){
+      (v.emplace_back(std::get<2*Is>(lists_tuple), std::get<2*Is+1>(lists_tuple)), ...);
+    };
+    create_pairs(std::make_index_sequence<sizeof...(lists)/2>());
+  }
+}
+
 int main (int argc, char *argv[]) {
   A a({1,2,3},{4,5,6},{7,8});
+  B b({1,2,3},{4,5,6},{7,8},{9,10});
+  B bx({1,2,3},{4},{5,6});
 
   for(auto& v : a.v){
     for(auto& i : v){
       std::cout << i << ' ';
     }
     std::cout << '\n';
+  }
+
+  for(auto& v : b.v){
+    std::cout << "[{";
+    for(auto& i : v.fst){
+      std::cout << i << ' ';
+    }
+
+    std::cout << "}{";
+    for(auto& i : v.snd){
+      std::cout << i << ' ';
+    }
+    
+    std::cout << "}]\n";
   }
   return 0;
 }
